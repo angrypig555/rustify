5863052a42a38ea6c96b116ad819e6e2c6f3f175
angrypig555 <brny@brny.dev>
2026-09-01 10:30:47 +0100

    begin work

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,47 @@
-fn main() {
-    println!("Hello, world!");
+use std::{env, io};
+use std::fs::File;
+use std::io::{BufRead, BufReader, BufWriter};
+
+const WARN: &str = "[\x1b[33mWARN\x1b[0m]";
+const OK: &str = "[\x1b[32mOK\x1b[0m]";
+const FAIL: &str = "[\x1b[31mFAIL\x1b[0m]";
+
+fn main() -> io::Result<()>{
+    let args: Vec<String> = env::args().collect();
+    if args.iter().count() < 3 {
+        panic!("{FAIL} Not enough arguments\nUsage: rustify [c_source_file] [output_file_name]");
+    }
+    println!("rustify - Licensed under the MIT license. Copyright (c) 2026 angrypig555");
+    let file_raw = match File::open(&args[1]) {
+        Ok(f) => f,
+        Err(e) => {
+            panic!("{FAIL} Failed to open file! {e}");
+        }
+    };
+    println!("{OK} Opened file {}", &args[1]);
+    let file_rs_raw = match File::create(&args[2]) {
+        Ok(f) => f,
+        Err(e) => {
+            panic!("{FAIL} Failed to create file! {e}");
+        }
+    };
+    println!("{OK} Created file {}", &args[2]);
+    let file_cc = BufReader::new(file_raw);
+    let mut file_rs = BufWriter::new(file_rs_raw);
+    for (num, line) in file_cc.lines().enumerate() {
+        let data = line?;
+        match data.as_str() {
+            "#include <iostream>" => {
+                println!("{OK} File imports iostream, nothing to do");
+            }
+            "int main() {" => {
+                println!("{OK} Found main function");
+                writeln!(file_rs, "fn main() {");
+            }
+            _ => {
+                println!("{WARN} Unknown function detected, skipping");
+            }
+        }
+    }
+    Ok(())
 }
---
+++ test/hello.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main() {
+    int foo = 1;
+    int bar = 2;
+    std::cout << "Hello World" << std::endl;
+    return 0;
+}
